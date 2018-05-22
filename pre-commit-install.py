b93ed9ba1abd095960c2cf60ed333b21b6fce80b
Owen Synge <osynge@suse.com>
2018-05-22 10:54:09 +0200

    Added logging
    Now the code loggs what it does so you can potentially reverse the operation.
    
    Signed-off-by: Owen Synge <owen.synge@jaysnest.de>

--- pre-commit-install.py
+++ pre-commit-install.py
@@ -2,10 +2,12 @@
 
 import os
 import argparse
+import logging
+import sys
 
 
 def find_git_repos(search_path):
-    repos = []
+    log = logging.getLogger("find_git_repos")
     for root, dir_names, file_names in os.walk(search_path):
         for dir_name in dir_names:
             path = os.path.join(root, dir_name)
@@ -15,27 +17,34 @@ def find_git_repos(search_path):
             base_split = os.path.split(path_split[0])
             if not base_split[1] == '.git':
                 continue
-            repos.append(base_split[0])
-            yield base_split[0]
+            repo_path = base_split[0]
+            log.debug("Found git repo:%s" % repo_path)
+            yield repo_path
 
 
 def find_git_hook_needed(link_src, search_path):
-    search_path = '/home/oms101/home/programming/'
+    log = logging.getLogger("find_git_hook_needed")
     for i in find_git_repos(search_path):
         hook_path = os.path.join(i, '.git/hooks/pre-commit')
         if os.path.exists(hook_path):
             # File exists or symbolic link
+            log.debug('hook exists:%s' % (hook_path))
             continue
         if os.path.islink(hook_path):
             # remove broken symlinks
+            log.warning('Removing broken symlink:%s' % (hook_path))
             os.remove(hook_path)
+        log.info('Creating symlink:%s' % (hook_path))
         os.symlink(link_src, hook_path)
 
 
 def get_link_src():
     abs_path = os.path.abspath(__file__)
     abs_path_dir = os.path.dirname(abs_path)
-    return os.path.join(abs_path_dir, 'pre-commit.py')
+    src = os.path.join(abs_path_dir, 'pre-commit.py')
+    log = logging.getLogger("get_link_src")
+    log.debug('Git hook src:%s' % (src))
+    return src
 
 
 def get_parser():
@@ -47,18 +56,56 @@ def get_parser():
         action='store',
         help='Base path to search for git repos.',
         metavar='DIR')
+    parser.add_argument('--verbose', '-v', action='count')
+    parser.add_argument('--quiet', '-q', action='count')
+    parser.add_argument("--logcfg", type=str, help="log configuration file")
     return parser
 
 
 def main(args=None, namespace=None):
     parser = get_parser()
     args = parser.parse_args(args=args, namespace=namespace)
-    link_src = get_link_src()
+
+    # Set up log file
+    LoggingLevel = logging.WARNING
+    LoggingLevelCounter = 1
+    logFile = None
+    if args.verbose:
+        LoggingLevelCounter = LoggingLevelCounter - args.verbose
+    if args.quiet:
+        LoggingLevelCounter = LoggingLevelCounter + args.quiet
+    if LoggingLevelCounter <= 0:
+        LoggingLevel = logging.DEBUG
+    if LoggingLevelCounter == 1:
+        LoggingLevel = logging.INFO
+    if LoggingLevelCounter == 2:
+        LoggingLevel = logging.WARNING
+    if LoggingLevelCounter == 3:
+        LoggingLevel = logging.ERROR
+    if LoggingLevelCounter == 4:
+        LoggingLevel = logging.FATAL
+    if LoggingLevelCounter >= 5:
+        LoggingLevel = logging.CRITICAL
+
+    if args.logcfg:
+        logFile = args.logcfg
+    if logFile is not None:
+        if os.path.isfile(logFile):
+            logging.config.fileConfig(logFile)
+        else:
+            logging.basicConfig(level=LoggingLevel)
+            log = logging.getLogger("main")
+            log.error("Logfile configuration file '%s' was not found." % (args.log_config))
+            sys.exit(1)
+    else:
+        logging.basicConfig(level=LoggingLevel)
+    log = logging.getLogger("main")
     base_dir = os.getcwd()
     if args.base:
         base_dir = args.base
-    
+    link_src = get_link_src()
     find_git_hook_needed(link_src, base_dir)
 
 
-main()
+if __name__ == "__main__":
+    main()
